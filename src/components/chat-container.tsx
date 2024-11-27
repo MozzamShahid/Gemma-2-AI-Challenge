131bdb9e1b771ee40d6024404a7db9e4e49e2e09
Dannah Ade <daede25@outlook.com>
2024-11-27 12:18:39 +0000

    Update chat-container.tsx

--- src/components/chat-container.tsx
+++ src/components/chat-container.tsx
@@ -4,11 +4,21 @@ import { ChatInput } from "./chat-input"
 
 export function ChatContainer() {
   return (
-    <div className="flex-1 flex flex-col">
-      <div className="ml-20 mr-20 mt-3"><ChatHeader /></div>
-      <ChatMessages />
-      <ChatInput />
+    <div className="flex-1 flex flex-col bg-[#1a0533] text-white rounded-xl shadow-xl overflow-hidden transition-all ease-in-out duration-300">
+      {/* Chat Header */}
+      <div className="ml-6 mr-6 mt-6 mb-4">
+        <ChatHeader />
+      </div>
+      
+      {/* Chat Messages */}
+      <div className="flex-1 overflow-auto bg-[#2e0b4e] rounded-lg p-4 mb-4 shadow-md">
+        <ChatMessages />
+      </div>
+      
+      {/* Chat Input */}
+      <div className="p-4 bg-[#1a0533] border-t border-white/30 transition-all hover:border-white/50">
+        <ChatInput />
+      </div>
     </div>
   )
 }
-
